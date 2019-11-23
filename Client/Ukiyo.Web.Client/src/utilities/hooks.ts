00cc0fab531714ecaa1de73f8916ad6847da7ea0
Ukiyo Roa <jnpco.98@gmail.com>
2019-11-23 19:45:11 +1100

    Added useinterval and clickoutside hooks

---
+++ Client/Ukiyo.Web.Client/src/utilities/hooks.ts
@@ -0,0 +1,40 @@
+import { useEffect, MutableRefObject } from 'react';
+
+export const useInterval = (callback: Function, delay: number): void => {
+    let currentCallback: Function;
+    let id = -1;
+
+    useEffect(() => {
+        currentCallback = callback;
+    }, [callback]);
+
+    useEffect(() => {
+        if (delay < 0) {
+            clearInterval(id);
+            id = -1;
+            return null;
+        }
+
+        id = setInterval(() => currentCallback(), delay);
+        return (): void => clearInterval(id);
+    }, [delay]);
+};
+
+export const useOnClickOutside = (ref: MutableRefObject<Node>, handler: Function): void => {
+    useEffect(() => {
+        const listener = (event: Event): void => {
+            if (!ref.current || ref.current.contains(event.target as Node)) {
+                return;
+            }
+            handler(event);
+        };
+
+        document.addEventListener('mousedown', listener);
+        document.addEventListener('touchstart', listener);
+
+        return (): void => {
+            document.removeEventListener('mousedown', listener);
+            document.removeEventListener('touchstart', listener);
+        };
+    }, [ref, handler]);
+};
