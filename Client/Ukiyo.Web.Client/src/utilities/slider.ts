caa98a7e843c03c50b7e68f96bbca4d79e7de4a8
Ukiyo Roa <jnpco.98@gmail.com>
2019-12-29 15:25:46 +1100

    Slider component

---
+++ Client/Ukiyo.Web.Client/src/utilities/slider.ts
@@ -0,0 +1,14 @@
+import { Settings } from 'react-slick';
+
+export const DEFAULT_SLIDER_SETTINGS: Settings = {
+    adaptiveHeight: true,
+    arrows: true,
+    autoplay: false,
+    cssEase: 'ease-in',
+    infinite: false,
+    initialSlide: 0,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipe: true,
+    touchMove: true
+};
