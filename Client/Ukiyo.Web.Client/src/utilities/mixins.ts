8303fed9dc5efdb1a70f576049147e8c383a9f21
Ukiyo Roa <jnpco.98@gmail.com>
2019-12-30 13:08:52 +1100

    Refactored to support js truncate media query

--- Client/Ukiyo.Web.Client/src/utilities/mixins.ts
+++ Client/Ukiyo.Web.Client/src/utilities/mixins.ts
@@ -62,17 +62,45 @@ export const padding = (top: string, right: string, bottom: string, left: string
   ${left && css` padding-left: ${left}; `};
 `;
 
-export const truncate = (fontSize: string, lineHeight: number = 1, linesToShow: number = 1, maxWidth: string = '100%') => css`
+type TruncateResponsiveSettings = {
+  default: { fontSize: string };
+  responsive?: { screenSize: string, fontSize?: string }[];
+}
+
+export const truncate = (fontSize: string | TruncateResponsiveSettings, lineHeight: number = 1, linesToShow: number = 1, maxWidth: string = '100%') => css`
   display: block;
   display: -webkit-box;
   max-width: ${maxWidth};
-  height: ${math(`${fontSize} * ${lineHeight} * ${linesToShow}`)};
-  font-size: ${fontSize};
   line-height: ${lineHeight};
   overflow: hidden;
   text-overflow: ellipsis;
   -webkit-line-clamp: ${linesToShow};
   -webkit-box-orient: vertical;
+  
+  ${typeof fontSize === 'string' &&
+    css`
+      height: ${math(`${fontSize} * ${lineHeight} * ${linesToShow}`)};
+      font-size: ${fontSize};
+    `
+  };
+
+  ${typeof fontSize === 'object' &&
+    css`
+      height: ${math(`${fontSize.default.fontSize} * ${lineHeight} * ${linesToShow}`)};
+      font-size: ${fontSize.default.fontSize};
+
+      ${fontSize.responsive && fontSize.responsive.length &&
+        fontSize.responsive.map(fr => 
+          css`
+            ${fr.screenSize} {
+              height: ${math(`${fr.fontSize} * ${lineHeight} * ${linesToShow}`)};
+              font-size: ${fr.fontSize};
+            }
+          `
+        )
+      }
+    `
+  };
 `;
 
 export const gradient = (degrees: string, colors: []) => css`
