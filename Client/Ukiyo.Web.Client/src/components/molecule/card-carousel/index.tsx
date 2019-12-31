c96ec7682fe5377647052f37c1b2dc2936cb1621
Ukiyo Roa <jnpco.98@gmail.com>
2019-12-31 12:23:52 +1100

    Card carousel styles

--- Client/Ukiyo.Web.Client/src/components/molecule/card-carousel/index.tsx
+++ Client/Ukiyo.Web.Client/src/components/molecule/card-carousel/index.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from 'react';
+import { Settings } from 'react-slick';
 import Card from '../../atom/card';
 import * as S from './style';
+import { DEFAULT_SLIDER_SETTINGS } from '../../../utilities/slider';
+import { SMALL } from '../../../settings/media';
 
 type Props = {
     content: {
@@ -15,20 +18,46 @@ type Props = {
 const CardCarousel: React.FC<Props> = (props: Props): ReactElement => {
     const { content } = props;
 
+    const sliderOptions: Settings = {
+        ...DEFAULT_SLIDER_SETTINGS,
+        slidesToShow: 3,
+        swipeToSlide: true,
+        centerMode: true,
+        prevArrow: <S.Arrow />,
+        nextArrow: <S.Arrow />,
+        responsive: [
+            {
+                breakpoint: +SMALL.replace(/\D/g, ''),
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 3,
+                    centerMode: false
+                }
+            }
+        ]
+    };
+
+    /* eslint-disable react/jsx-props-no-spreading */
     return (
-        <div style={{ display: 'flex' }}>
-            {content.map(({ heading, subtitle, genre, link, image }) => (
-                <Card
-                    key={heading + subtitle}
-                    heading={heading}
-                    subtitle={subtitle}
-                    genre={genre}
-                    link={link}
-                    image={image}
-                />
-            ))}
-        </div>
+        <S.Container>
+            <S.Slider {...sliderOptions}>
+                {content.map(({ heading, subtitle, genre, link, image }) => (
+                    <S.ItemWrapper className="wrapper" key={heading + subtitle}>
+                        <S.Item
+                            key={heading + subtitle}
+                            heading={heading}
+                            subtitle={subtitle}
+                            genre={genre}
+                            link={link}
+                            image={image}
+                        />
+                    </S.ItemWrapper>
+                ))}
+            </S.Slider>
+        </S.Container>
     );
+    /* eslint-enable react/jsx-props-no-spreading */
 };
 
 export default CardCarousel;
--- Client/Ukiyo.Web.Client/src/components/molecule/card-carousel/style.ts
+++ Client/Ukiyo.Web.Client/src/components/molecule/card-carousel/style.ts
@@ -1,4 +1,71 @@
 import styled, { css, keyframes } from 'styled-components';
+import CustomSlickArrow from '../../atom/slick-arrow';
+import Slick from 'react-slick';
 import * as M from '../../../settings/media';
+import Card from '../../atom/card';
 
-export default '';
+export const Container = styled.div`
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+
+  &:before, &:after {
+    /* content: ''; */
+    position: absolute;
+    width: 8%;
+    height: 100%;
+    top: -8%;
+  }
+
+  &:before {
+    left: 0;
+    background: linear-gradient(to left, rgba(22, 22, 34, 0) 0%, rgba(22, 22, 34, 1) 100%);
+  }
+
+  &:after {
+    right: 0;
+    background: linear-gradient(to right, rgba(22, 22, 34, 0) 0%, rgba(22, 22, 34, 1) 100%);
+  }
+`;
+
+export const ItemWrapper = styled.div`
+  position: relative;
+  padding-bottom: 60%;
+`;
+
+export const Item = styled(Card)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: unset;
+  height: unset;
+  width: 45%;
+`;
+
+export const Slider = styled(Slick)`
+  margin-bottom: 2rem;
+  
+  .slick-dots {
+    left: 0;
+    bottom: -2rem;
+
+    li {
+      margin: 0;
+
+      button:before {
+        color: white;
+      }
+    }
+  }
+
+  /* padding: 0 2rem; */
+
+  .slick-list {
+    overflow: visible;
+  }
+`;
+
+export const Arrow = styled(CustomSlickArrow)`
+`;
