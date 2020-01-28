0b32a1a16109c3ddae3583b9aa973995601ce5cb
Ukiyo Roa <jnpco.98@gmail.com>
2020-01-28 22:11:34 +1100

    Finishing up contact page

---
+++ Client/Ukiyo.Web.Client/src/components/template/contact/index.tsx
@@ -0,0 +1,45 @@
+import React, { ReactElement } from 'react';
+import * as S from './style';
+import DynamicHTML from '../../molecule/dynamic-html';
+import { contact } from '../../../settings/config/settings.json';
+import { jsonFormData } from '../../../utilities/formdata';
+import { AnyStyledComponent } from 'styled-components';
+
+const Contact: React.FC = (): ReactElement => {
+    const { pageHeading, pageText, fields, submitText } = contact;
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        if(!(event.target instanceof HTMLFormElement)) return;
+
+        const formData = new FormData(event.target);
+
+        const params = {
+            ...jsonFormData(formData),
+            ['Source']: window.location.href
+        };
+
+        console.log('call api to submit', params);
+    }
+
+    return (
+        <S.ContactPageContainer>
+            <S.ContactPageContent>
+                {pageHeading && <S.ContactPageTitle>{pageHeading}</S.ContactPageTitle>}
+                {pageText && <DynamicHTML HTMLString={pageText}/>}
+            </S.ContactPageContent>
+            <S.ContactPageForm onSubmit={handleSubmit}>
+                {
+                    fields.map(f => {
+                        const InputStyledComponent: AnyStyledComponent = f.multi ? S.ContactPageFormTextBox : S.ContactPageFormInput;
+                        return <InputStyledComponent name={f.displayName} placeholder={f.placeholder} required={f.required} pattern={f.pattern || null} autocomplete="off"/>
+                    })
+                }
+                <S.ContactPageFormSubmit submitButton>{submitText}</S.ContactPageFormSubmit>
+            </S.ContactPageForm>
+        </S.ContactPageContainer>
+    );
+};
+
+export default Contact;
---
+++ Client/Ukiyo.Web.Client/src/components/template/contact/style.ts
@@ -0,0 +1,79 @@
+import styled, { css } from 'styled-components/macro';
+import { gutter, GUTTER_LEFT, GUTTER_RIGHT, GUTTER_TOP } from '../../../utilities/mixins';
+import { SectionTitle } from '../../atom/text/style';
+import * as M from '../../../settings/media';
+import Button from '../../atom/button';
+import Input, { InputType } from '../../atom/input';
+
+const ContactPageFormText = css`
+  border: 0;
+  background: none;
+  padding: 0.5rem;
+  appearance: none;
+  width: 100%;
+  margin: 0;
+  background: white;
+  padding: 1rem 1.5rem;
+  margin-bottom: 1rem;
+  transition: all 0.1s ease;
+`;
+
+export const ContactPageContainer = styled.div`
+  ${gutter(GUTTER_LEFT)};
+  ${gutter(GUTTER_RIGHT)};
+  ${gutter(GUTTER_TOP)};
+
+  width: 100%;
+`;
+
+export const ContactPageContent = styled.div`
+  & + & {
+    margin-top: 3rem;
+  }
+
+  ${M.MEDIA_SMALL} {
+    & + & {
+      margin-top: 5rem;
+    }
+  }
+`;
+
+export const ContactPageTitle = styled(SectionTitle).attrs({ as: 'h1' })``;
+
+export const ContactPageForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem 0;
+`;
+
+type ContactPageFieldStyleProps = {
+  name?: string;
+  placeholder?: string;
+  required?: boolean;
+  pattern?: string;
+}
+
+export const ContactPageFormInput = styled(Input).attrs({ inputType: InputType.Single })<ContactPageFieldStyleProps>`
+  ${ContactPageFormText};
+  
+  height: 2.5rem;
+
+  ${M.MEDIA_LARGE} {
+    height: 3rem;
+  }
+`;
+
+export const ContactPageFormTextBox = styled(Input).attrs({ inputType: InputType.Multi })<ContactPageFieldStyleProps>`
+  ${ContactPageFormText};
+  resize: none;
+  min-height: 10rem;
+  
+  ${M.MEDIA_LARGE} {
+    height: 12rem;
+  }
+`;
+
+export const ContactPageFormSubmit = styled(Button)`
+  align-self: start;
+  margin: 1rem 0;
+`;
