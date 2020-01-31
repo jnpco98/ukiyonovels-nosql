e6063d7fe7eb340f94c5375c53052400ad7d9f0f
Ukiyo Roa <jnpco.98@gmail.com>
2020-02-01 05:49:38 +1100

    Finished the footer

--- Client/Ukiyo.Web.Client/src/components/organism/footer/index.tsx
+++ Client/Ukiyo.Web.Client/src/components/organism/footer/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Accordion from '../../molecule/accordion';
-import Classifications from '../../molecule/classifications';
 import * as S from './style';
+import { useMediaQuery } from 'react-responsive';
+import { SMALL } from '../../../settings/media';
+import Text from '../../atom/text';
 
 const genre = ["Adventure", "Action", "Fantasy", "Martial Arts", "Mature", "Romance", "Seinen", "Supernatural", "Xianxia", "Adventure", "Action", "Fantasy", "Martial Arts", "Mature", "Romance", "Seinen", "Supernatural", "Xianxia"];
 const classification = ["Light novel", "Web novel"];
@@ -10,17 +11,37 @@ const status = ['Complete', 'Ongoing', 'Hiatus'];
 const generateLink = (strs: string[]) => strs.map(str => ({ name: str, link: '#' }));
 
 const content = [
-    { heading: "Genre", content: <Classifications classifications={generateLink(genre)}/> },
-    { heading: "Novel Type", content: <Classifications classifications={generateLink(classification)}/> },
-    { heading: "Status", content: <Classifications classifications={generateLink(status)}/> }, 
+    { heading: "Genre", content: <S.FooterGenreClassifications headingText="Genre" classifications={generateLink(genre)}/> },
+    { heading: "Status", content: <S.FooterClassifications headingText="Status" classifications={generateLink(status)}/> }, 
+    { heading: "Novel Type", content: <S.FooterClassifications headingText="Novel type" classifications={generateLink(classification)}/> },
 ]
 
 const Footer = () => {
+    const isSmallScreen = useMediaQuery({ minWidth: SMALL });
+
     return (
         <S.FooterContainer>
-            <S.FooterSearchLinks>
-                <S.FooterSearchLinksAccordion accordionContent={content} initialOpen={false}/>
-            </S.FooterSearchLinks>
+            {
+                !isSmallScreen ? 
+                    <S.FooterAccordionWrapper>
+                        <S.FooterAccordion accordionContent={content} initialOpen={false}/>
+                    </S.FooterAccordionWrapper> :
+                    <S.FooterClassificationLinksWrapper>
+                        {content.map(c => c.content)}
+                    </S.FooterClassificationLinksWrapper>
+            }
+
+            <S.FooterContactWrapper>
+                <S.FooterLinks>Contact</S.FooterLinks>
+                <S.FooterContactText>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius neque quibusdam tempore saepe pariatur voluptates, ut eligendi! Autem doloribus deleniti harum neque, quidem ea commodi voluptates, cupiditate quia explicabo hic ullam? Dolor impedit facilis modi veritatis dolore deleniti cupiditate rem iure id ex temporibus provident expedita, tempore enim nisi animi aperiam aliquam voluptatem suscipit!</S.FooterContactText>
+            </S.FooterContactWrapper>
+
+            <S.FooterLinksWrapper>
+                <S.FooterLinks>&copy; 2020 UkiyoNovels</S.FooterLinks>
+                <S.FooterLinks>Terms &amp; conditions</S.FooterLinks>
+                <S.FooterLinks>Privacy policy</S.FooterLinks>
+                <S.FooterLinks>Copyright</S.FooterLinks>
+            </S.FooterLinksWrapper>
         </S.FooterContainer>
     );
 };
--- Client/Ukiyo.Web.Client/src/components/organism/footer/style.ts
+++ Client/Ukiyo.Web.Client/src/components/organism/footer/style.ts
@@ -1,21 +1,105 @@
-import styled from 'styled-components';
-import { gutter, GUTTER_LEFT, GUTTER_RIGHT } from '../../../utilities/mixins';
+import styled from 'styled-components/macro';
+import { gutter, GUTTER_LEFT, GUTTER_RIGHT, GUTTER_BOTTOM } from '../../../utilities/mixins';
 import Accordion from '../../molecule/accordion';
-import { AccordionContainer, AccordionTabLabel } from '../../molecule/accordion/style';
+import Text, { TextType } from '../../atom/text';
+import * as M from '../../../settings/media';
+import { ClassificationsList, ClassificationsItem, ClassificationsHeading } from '../../molecule/classifications/style';
+import Classifications from '../../molecule/classifications';
 
 export const FooterContainer = styled.div`
   ${gutter(GUTTER_LEFT)};
   ${gutter(GUTTER_RIGHT)};
-  padding-top: 1rem;
+
+  padding-top: 2rem;
+  padding-bottom: 3rem;
+
   width: 100%;
   background: ${({ theme, ...props }) => theme.colors.white};
+
+	${M.MEDIA_SMALL} {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	${M.MEDIA_XLARGE} {
+		padding-top: 3rem;
+	}
 `;
 
-export const FooterSearchLinks = styled.div`
+export const FooterAccordionWrapper = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+	margin-bottom: 1.5rem;
 `;
 
-export const FooterSearchLinksAccordion = styled(Accordion).attrs({ flat: true })`
+export const FooterAccordion = styled(Accordion).attrs({ flat: true })`
+	${ClassificationsHeading} {
+		display: none;
+	}
+
+	${M.MEDIA_SMALL} {
+		display: block;
+	}
+`;
+
+export const FooterLinksWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 1rem;
+
+	${M.MEDIA_SMALL} {
+		flex-direction: row;
+		justify-content: space-between;
+		margin-top: 3rem;
+	}
+`;
+
+export const FooterContactText = styled(Text)`
+	max-width: 40rem;
+
+	${M.MEDIA_XLARGE} {
+		max-width: 62rem;
+	}
+`;
+
+export const FooterContactWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 1rem;
+
+	${M.MEDIA_SMALL} {
+		margin-top: 3rem;
+	}
+`;
+
+export const FooterLinks = styled(Text).attrs({ textType: TextType.Anchor })`
+	text-transform: uppercase;
+	margin-right: 3rem;
+`
+
+export const FooterClassificationLinksWrapper = styled.div`
+	${M.MEDIA_SMALL} {
+		display: flex;
+		justify-content: space-between;
+	}
+`;
+
+export const FooterClassifications = styled(Classifications)`
+	width: 12%;
+`;
+
+export const FooterGenreClassifications = styled(Classifications)`
+	width: 60%;
+
+	${ClassificationsList} {
+		flex-wrap: wrap;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+	${ClassificationsItem} {
+		width: 30%;
+	}
 `;
