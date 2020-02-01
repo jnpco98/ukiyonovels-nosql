c164da8385f09d7aff1c9ecf4e5fc4233df11b01
Ukiyo Roa <jnpco.98@gmail.com>
2020-02-02 00:45:22 +1100

    Added comma model binder

---
+++ Infrastructure/Ukiyo.Infrastructure/ModelBinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Ukiyo.Infrastructure
+{
+    public class CommaSeparatedModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var modelName = bindingContext.ModelName;
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if(valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var stringArray = valueProviderResult.FirstValue?.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList();
+
+            if(stringArray.Count <= 0)
+            {
+                return Task.CompletedTask;
+            }
+            bindingContext.Result = ModelBindingResult.Success(stringArray);
+
+            return Task.CompletedTask;
+        }
+    }
+}
